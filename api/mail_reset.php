1fd1a4f256f4337045b200205959a69f6748ab88
Palomotaku <francoitalo4@hotmail.com>
2023-11-11 20:58:08 -0500

    Update mail_reset.php

--- api/mail_reset.php
+++ api/mail_reset.php
@@ -2,7 +2,7 @@
 
 if(isset($_POST['enviar'])) {
     if(!empty($_POST['nom_']) && !empty($_POST['apelli_']) && !empty($_POST['corre_']) && 
-    !empty($_POST['contra_']) && !empty($_POST['cel_']) {
+    !empty($_POST['contra_']) && !empty($_POST['cel_'])) {
         $nom_ = $_POST['nom_'];
         $apelli_ = $_POST['apelli_'];
         $corre_ = $_POST['corre_'];
