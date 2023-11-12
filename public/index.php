3a2cfa284cfef997e1c1fe7c10b25b9710026bf1
Palomotaku <francoitalo4@hotmail.com>
2023-11-12 02:32:46 -0500

    Update index.php

--- public/index.php
+++ public/index.php
@@ -11,7 +11,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Stryfix - Login</title>
-    <link href="../css/style.css" rel="stylesheet">
+    <link href="https://public-html-ten.vercel.app/public/css/style.css" rel="stylesheet">
     <script src="js/login.js"></script>
 </head>
 <body>
@@ -31,13 +31,13 @@
                 </div>
                 <div class="Remember-Forgot">
                     <label><input type="checkbox"> Recordar mi cuenta</label>
-                    <a href="remember.html">Olvido su Contraseña?</a>
+                    <a href="../remember.html">Olvido su Contraseña?</a>
                 </div>
                 <div class="Log-In">
                     <input type="submit" value="Iniciar Sesion">
                 </div>
                 <div class="Register">
-                    No estas registrado? <a href="registrarse.php">Registrate</a>
+                    No estas registrado? <a href="../registrarse.php">Registrate</a>
                 </div>
             </form>
         </div>
