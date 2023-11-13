fe842664a8130f117e92959f29ed118de7c4775a
Palomotaku <francoitalo4@hotmail.com>
2023-11-13 01:53:49 -0500

    Update conexion.php

--- php/conexion.php
+++ php/conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-    $conexion = mysqli_connect("bxr9l2qqbuyh852lu3na-mysql.services.clever-cloud.com", "bxr9l2qqbuyh852lu3na", "ueqli12irmyfucjv", "W8UMOfkcAU14oDbkPMGH");
+    $conexion = mysqli_connect("bxr9l2qqbuyh852lu3na-mysql.services.clever-cloud.com", "ueqli12irmyfucjv", "W8UMOfkcAU14oDbkPMGH", "bxr9l2qqbuyh852lu3na");
 
     // if($conexion){
     //     echo 'CONEXION EXITO MI KING';
