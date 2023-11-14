5848a1a9373c0ad329818eae2bde717465dda9e9
Palomotaku <francoitalo4@hotmail.com>
2023-11-14 17:50:52 -0500

    Update login.php

--- php/login.php
+++ php/login.php
@@ -12,7 +12,7 @@
 
 
     if (mysqli_num_rows($valid_login) > 0){
-        $_SESSION['usuario'] = $corre_;
+        $_SESSION["USER"] = $corre_;
 
 
         header("location: ../public/inicial.php");
