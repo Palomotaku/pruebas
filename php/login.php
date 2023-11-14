42fc74a0876473f2b8ceec52b685dc2da7fe837f
Palomotaku <francoitalo4@hotmail.com>
2023-11-14 18:01:11 -0500

    .

--- php/login.php
+++ php/login.php
@@ -12,7 +12,7 @@
 
 
     if (mysqli_num_rows($valid_login) > 0){
-        $_SESSION["USER"] = 100;
+        $_SESSION["USER"] = $corre_;
 
 
         header("location: ../public/inicial.php");
--- public/inicial.php
+++ public/inicial.php
@@ -1,5 +1,7 @@
 <?php
     session_start();
+
+    include("login.php");
     $user = $_SESSION["USER"];
 
     echo $user . "<br/>";
