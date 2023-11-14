d8c377d5e7dee291adfd936fcf2a2653afdeb87f
Palomotaku <francoitalo4@hotmail.com>
2023-11-14 18:58:39 -0500

    Update login.php

--- php/login.php
+++ php/login.php
@@ -4,30 +4,62 @@
 
     include 'conexion.php';
 
-    $corre_ = isset($_POST['corre_']) ? $_POST['corre_'] : '';
-    $contra_ = isset($_POST['contra_']) ? $_POST['contra_'] : '';
-    $contra_ = hash('sha512', $contra_ ?? '');
-
-    $valid_login = mysqli_query($conexion, "SELECT * FROM usuario WHERE corre_='$corre_' and contra_='$contra_'");
+      $corre_ = strip_tags($_POST['corre_']);
+      $corre_ = trim($_POST['corre_']);
 
 
-    if(!$valid_login){
-        die("Error en la conexion: ");
-    }
-    if (mysqli_num_rows($valid_login) > 0){
-        $_SESSION['USER'] = $corre_;
+      $contra_ = strip_tags(md5($_POST['contra_']));
+      $contra_ = trim(md5($_POST['contra_']));
+
+      $query = mysqli_query($conexion, "SELECT * FROM usuarios WHERE corre_ = '$corre_' AND contra_ = '$contra_'");
+      $contar = mysqli_num_rows($query);
+
+      if($contar == 1) 
+
+      {
+
+        while($row=mysqli_fetch_array($query)) 
+
+        {
+
+          if($usuario = $row['usuario'] && $contrasena = $row['contrasena'])
+
+          {
+
+            $_SESSION['usuario'] = $usuario;
+            $_SESSION['id'] = $row['id_use'];
+
+            header('Location: index.php');
+
+          }
+
+        }
+        
+      } else { echo 'Los datos ingresados no son correctos'; }
 
 
-        header("location: ../public/inicial.php");
-        exit;
-    }else{
-        echo'
-           <script>
-               alert("Cuenta no existente");
-               window.location = "../public/index.php";
-          </script>
-        ';
-        exit;
-    }
+
+    // $corre_ = isset($_POST['corre_']) ? $_POST['corre_'] : '';
+    // $contra_ = isset($_POST['contra_']) ? $_POST['contra_'] : '';
+    // $contra_ = hash('sha512', $contra_ ?? '');
+
+    // $valid_login = mysqli_query($conexion, "SELECT * FROM usuario WHERE corre_='$corre_' and contra_='$contra_'");
+
+
+    // if (mysqli_num_rows($valid_login) > 0){
+    //     $_SESSION['USER'] = $corre_;
+
+
+    //     header("location: ../public/inicial.php");
+    //     exit;
+    // }else{
+    //     echo'
+    //        <script>
+    //            alert("Cuenta no existente");
+    //            window.location = "../public/index.php";
+    //       </script>
+    //     ';
+    //     exit;
+    // }
 
 ?>
