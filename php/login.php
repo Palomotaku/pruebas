3537940bc916551046e760ae8a7250d58ff8d040
Palomotaku <francoitalo4@hotmail.com>
2023-11-18 10:00:43 -0500

    Update login.php

--- php/login.php
+++ php/login.php
@@ -12,18 +12,11 @@
       $query = mysqli_query($conexion, "SELECT * FROM usuario WHERE corre_ = '$corre_' AND contra_ = '$contra_'");
       $contar = mysqli_num_rows($query);
 
-      if($contar == 1) 
-
-      {
-
-        while($row=mysqli_fetch_array($query)) 
-
+      if($contar == 1){
+        while($row=mysqli_fetch_array($query))
         {
-
           if($corre_ = $row['corre_'] && $contra_ = $row['contra_'])
-
           {
-
             $_SESSION['usuario'] = $row['corre_'];
             $_SESSION['nombre'] = $row['nom_'];
 
@@ -32,12 +25,8 @@
                 window.location = "../public/inicial.php";
             </script>
             ';
-            exit;
-
           }
-
-        }
-        
+          }     
       } else { 
         echo '
             <script>
@@ -45,32 +34,6 @@
                 window.location = "../public/index.php";
             </script>
             ';
-            exit;
     }
-
-
-
-    // $corre_ = isset($_POST['corre_']) ? $_POST['corre_'] : '';
-    // $contra_ = isset($_POST['contra_']) ? $_POST['contra_'] : '';
-    // $contra_ = hash('sha512', $contra_ ?? '');
-
-    // $valid_login = mysqli_query($conexion, "SELECT * FROM usuario WHERE corre_='$corre_' and contra_='$contra_'");
-
-
-    // if (mysqli_num_rows($valid_login) > 0){
-    //     $_SESSION['USER'] = $corre_;
-
-
-    //     header("location: ../public/inicial.php");
-    //     exit;
-    // }else{
-    //     echo'
-    //        <script>
-    //            alert("Cuenta no existente");
-    //            window.location = "../public/index.php";
-    //       </script>
-    //     ';
-    //     exit;
-    // }
-
+    
 ?>
